e4d823a3c583049150835a5d57f85ab2e9b17eed
Arpan <mastermind@altmails.com>
2023-07-06 01:12:00 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.render('index', { title: 'Home' });
+});
+
+app.listen(port, () => console.log(`app listening on port ${port}!`));
