c38de2919d8e2f0e9a31695bccd8cf488ddf03e1
Arpan <mastermind@altmails.com>
2023-07-06 16:37:27 +0530

    finishing touches part 2

---
+++ public/src/frontend.js
@@ -0,0 +1,83 @@
+
+body = document.getElementsByTagName('body')[0],
+
+bg = document.getElementById('background');
+
+classes = document.getElementsByClassName('same-colour-as-bg');
+
+function scroll() {
+  var newColor = window.scrollY/5;
+
+  bg.style.webkitFilter = "hue-rotate(" + newColor + "deg)";
+
+  for (i = 0; i < classes.length; i++) {
+
+      classes[i].style.webkitFilter = "hue-rotate(" + newColor + "deg)";
+
+  }
+
+}
+const leyen = document.getElementById('LEGEND');
+const atrav = document.getElementById('atrav-gaming');  
+
+width = screen.width;
+if(width <= 1000 || innerWidth <= 1000){
+  
+    leyen.addEventListener('mouseover', function() {
+      leyen.classList.add('animate-small-pc');
+  });
+  leyen.addEventListener('transitioned', function() {
+      leyen.classList.remove('animate-small-pc');
+  });
+
+    function checkCollision_small_pc(){
+      leyenpos = leyen.getBoundingClientRect()
+      atravLeft = atrav.getBoundingClientRect().left
+      atravRight = atrav.getBoundingClientRect().right
+      console.log(leyenpos.right + " " + atravLeft)
+      if (leyenpos.right >= (atravLeft)) {
+        atrav.classList.add('animateAtrav-small-pc');  
+      } 
+      
+      setTimeout(checkCollision_small_pc, 10);
+  }(checkCollision_small_pc());
+
+
+}
+
+
+else if((width || innerWidth) <= 400){
+  document.getElementById("animation-container").remove();
+  console.log("removed")
+}
+
+else{
+
+
+  function checkCollision(){
+  leyenpos = leyen.getBoundingClientRect()
+  atravLeft = atrav.getBoundingClientRect().left
+  atravRight = atrav.getBoundingClientRect().right
+
+  if (leyenpos.right >= atravLeft) {
+    atrav.classList.add('animateAtrav');  
+  } 
+  
+  setTimeout(checkCollision, 10);
+}(checkCollision());
+
+leyen.addEventListener('mouseover', function() {
+  leyen.classList.add('animate');
+});
+leyen.addEventListener('transitioned', function() {
+  leyen.classList.remove('animate');
+});
+
+}
+
+obj = document.getElementsByClassName("screenshot")
+for (var i = 0; i < obj.length; i++) {
+obj[i].addEventListener('click', function() {
+window.open(this.src, '_blank');
+});
+}
