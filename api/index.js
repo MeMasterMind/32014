1835d1c730de1d9af637d424a61d36616fa2a0f8
Arpan <mastermind@altmails.com>
2024-03-07 01:07:36 +0530

    vercel

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ const path = require('path');
 
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, '../public')));
+app.set('views', path.join(__dirname, '../views'));
 
 app.get('/', (req, res) => {
     res.render('index', { name: '32014' });
